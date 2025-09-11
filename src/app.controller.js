9d596ce8e772f37658a3d6e812a87d685ba2a56e
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-09-11 15:04:55 +0300

    DB connected

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv'
 dotenv.config({path:path.join('./.env')})
 import express from 'express'
 import cors from 'cors'
+import connectDB from './config/connection.db.js';
 
 
 export const Bootstrap=async()=>{
@@ -14,7 +15,7 @@ const port=process.env.PORT
  app.use(express.json())
 
  //DB
-
+  await connectDB()
   //Routes
   app.get('/',(req,res)=>{
     res.json("app runn")
